33b8f9180cd0a8d6c03ea29b8d4f19aaf1aedef4
赵吉彤 <zjtong3576@sina.com>
2016-09-30 11:07:50 +0800

    上拉加载功能+没有快滚

--- pages/zhihu/zhihu.js
+++ pages/zhihu/zhihu.js
@@ -2,15 +2,54 @@
 var app = getApp()
 Page({
   data: {
-    pageTitle: '知乎日报',
     zhihuData: {},
     zhihuArr: [],
-    errImg: '../../images/tt.png'
+    newestDate: '',
+    errImg: '../../images/nogo.jpg'
   },
+  // 图片404触发事件
   ifImgErr: function(e) {
-    var i = e.currentTarget.id;
-    console.log(i)
+    var oldArr = this.data.zhihuArr;
+    var i = Number(e.currentTarget.id);
+
+    for(var j = 0 ; j < oldArr.length ; j++ ) {
+      if( i == j ) {
+        oldArr[i].images[0] = '../../images/nogo.jpg';
+      }
+    }
+
+    this.setData({
+      zhihuArr: oldArr
+    })
   },
+  // 下拉刷新
+  refreshLoad: function() {
+     console.log("触发下拉刷新")
+  },
+  // 上拉继续加载
+  continueLoad: function() {
+    var that = this
+    var checkDate = this.data.newestDate
+    var oldArr = this.data.zhihuArr
+    console.log("触发上拉继续加载")
+    wx.request({
+      url: 'http://news.at.zhihu.com/api/4/news/before/' + checkDate,
+      data: {},
+      success: function(res) {
+        if(res.errMsg == "request:ok") {
+          console.log(res)
+          var newArr = oldArr.concat(res.data.stories);
+          console.log(newArr)
+          that.setData({
+            zhihuArr: newArr
+          })
+        }else {
+          // 错误提示
+        }
+      }
+    })
+  },
+  // 点击跳转页面详情
   turnToDetailPage: function(e) {
     var newId = e.currentTarget.dataset.id
 
@@ -27,6 +66,7 @@ Page({
       }
     })
   },
+  // 页面加载
   onLoad: function () {
     var that = this
     wx.request({
@@ -37,16 +77,13 @@ Page({
         if(res.errMsg == "request:ok") {
           that.setData({
             zhihuData: res.data,
-            zhihuArr: res.data.stories
+            zhihuArr: res.data.stories,
+            newestDate: res.data.date
           })
         }else {
           // 错误提示
         }
       }
-    })
-
-    wx.setNavigationBarTitle({
-      title: this.data.pageTitle
     })
   }
 })
